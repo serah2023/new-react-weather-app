a4333e4ae94999ff7c83b112af1972d97e2cd888
serah2023 <123532441+serah2023@users.noreply.github.com>
2023-02-12 12:35:17 +0330

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
 
- export default function App ()
-  {
+export default function App() {
   return (
     <div className="App">
-      <footer>
-        This project is coded by sara zolfaghari and is 
-        <a
-          href="https://github.com/serah2023/new-react-weather-app"
-          target="_blank"
-          rel="noreferrer"> open sourced</a>
-  
-      </footer>
+      <div className="container">
+        <h1>weather app</h1>
+        <footer>
+          This project is coded by sara zolfaghari and is
+          <a
+            href="https://github.com/serah2023/new-react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            open sourced
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
-

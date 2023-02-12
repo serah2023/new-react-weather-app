371e8173b7fdddb52fd2781c43c8a0971d247811
serah2023 <123532441+serah2023@users.noreply.github.com>
2023-02-12 12:10:53 +0330

    add github link

--- src/App.js
+++ src/App.js
@@ -4,9 +4,14 @@ import './App.css';
   {
   return (
     <div className="App">
-        
-    hello
-
+      <footer>
+        This project is coded by sara zolfaghari and is 
+        <a
+          href="https://github.com/serah2023/new-react-weather-app"
+          target="_blank"
+          rel="noreferrer"> open sourced</a>
+  
+      </footer>
     </div>
   );
 }

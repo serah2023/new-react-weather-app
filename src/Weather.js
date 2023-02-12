6e43f19d64dfb4fd514959fa14fdabf15bcb3202
serah2023 <123532441+serah2023@users.noreply.github.com>
2023-02-12 18:01:36 +0330

    improved css

--- src/Weather.js
+++ src/Weather.js
@@ -1,20 +1,27 @@
 import React from "react";
+// import ReactAnimatedWeather from "react-animated-weather";
+
 import "./Weather.css";
 
 export default function Weather() {
   return (
-    <div className="Weather">
+    <div classNameName="Weather">
       <form>
-        <div className="row">
-          <div className="col-9">
+        <div classNameName="row mt-3">
+          <div classNameName="col-9">
             <input
               type="search"
               placeholder="enter a city.."
-              className="form-control"
+              classNameName="form-control"
+              autoFocus="on"
             />
           </div>
-          <div className="col-3">
-            <input type="submit" value="search" className="btn btn-primary" />
+          <div classNameName="col-3">
+            <input
+              type="submit"
+              value="search"
+              className="btn btn-primary w-100"
+            />
           </div>
         </div>
       </form>
@@ -28,19 +35,28 @@ export default function Weather() {
 
       <div className="row">
         <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/48/partly_cloudy.png"
-            alt="mostly cloudy"
-          ></img>
-          4°C
+          <div className="clearfix">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+              alt="Cloudy icon"
+              className="weather-icon float-left"
+            />
+            <div className="float-left">
+              <span className="temperature" >
+                19
+              </span>
+              <span className="unit">°C</span>
+            </div>
+          </div>
         </div>
-
         <div className="col-6">
           <ul>
-            <li>Precipitation: 2%</li>
-            <li>Humidity: 50%</li>
-
-            <li>Wind: 8 km/h</li>
+            <li>
+              Humidity: <span id="humidity"></span>%
+            </li>
+            <li>
+              Wind: <span id="wind"></span> km/h
+            </li>
           </ul>
         </div>
       </div>
